9f0285d504804320b74f00ef52a857856d2c05ce
honguyen25121991 <honguyen25121991@gmail.com>
2021-04-05 15:28:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.scss";
+import ColorBox from "./component";
 
 function App() {
   return (
     <div className="app">
       <h1>Welcome</h1>
       <h2>Xin chao</h2>
+      <ColorBox></ColorBox>
     </div>
   );
 }
